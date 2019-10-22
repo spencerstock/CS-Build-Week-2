8e6a08aac15c220d6987768ee3e274a49c7e6751
Spencer Stock <spencerstock@gmail.com>
2019-10-22 15:34:11 -0600

    BackendAPI added

---
+++ app/src/main/java/com/spencerstock/lambdahunt/Retrofit/BackendAPI.kt
@@ -0,0 +1,27 @@
+package com.spencerstock.lambdahunt.Retrofit
+
+import com.spencerstock.lambdahunt.Model.Direction
+import com.spencerstock.lambdahunt.Model.Room
+import io.reactivex.Observable
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface BackendAPI {
+    @get:Headers(
+        "Authorization: token //censored"
+    )
+    @get:GET("adv/init")
+    val rooms: Observable<Room>
+
+
+    @Headers(
+        "Content-Type: application/json",
+        "Authorization: token 89db1ddef40b9c538dac43796bbce93001a3ee49"
+    )
+    @POST("adv/move")
+    fun move(@Body body: Direction): Observable<Room>
+
+}
